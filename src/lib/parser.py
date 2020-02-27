c7637dfdac5050161d9f6865cf2ff9107e14fc83
Janus <j4nusl1n@gmail.com>
2020-02-27 17:26:19 +0800

    [Chg] rewrite domestic status parser

--- src/lib/parser.py
+++ src/lib/parser.py
@@ -4,7 +4,7 @@ from lxml import etree
 import re
 
 class DomesticStatus(object):
-
+    _time_table = None
     def __init__(self, page_str=None):
         if type(page_str) is not str:
             raise TypeError(
@@ -29,6 +29,8 @@ class DomesticStatus(object):
         if time_table is None:
             raise Exception('ParseError')
 
+        self._time_table = time_table
+
     def hasError(self):
         self.error_messages = []
         if len(self.page_tree.xpath('//div[@class="errorMessageBlockA01"]')):
@@ -43,13 +45,18 @@ class DomesticStatus(object):
         return False
 
     def getTimeTable(self):
+        if self._time_table is not None:
+            return self._time_table
+
         node_list = self.page_tree.xpath('//div[@id="completedTable-body"]//table')
         if len(node_list):
             return node_list[0]
         
         return None
 
-    def parseTimeTable(self, time_table):
+    def parseTimeTable(self, time_table=None):
+        if time_table is None:
+            time_table = self._time_table
         dict_time = {
             'flight': '',
         }
@@ -57,7 +64,7 @@ class DomesticStatus(object):
         for th in time_table.xpath('//thead//tr[1]//th'):
             key = th.text.strip().lower()
             dict_time[key] = ''
-            if key in ['departs', 'arrives']:
+            if key in ['departs', 'arrives', 'remarks']:
                 dict_time[key] = {}
 
         for th in time_table.xpath('//thead//tr[2]//th[position() < 5 and position() > 1]'):
@@ -68,29 +75,64 @@ class DomesticStatus(object):
             key = th.text.strip().lower()
             dict_time['arrives'][key] = ''
 
-        key_list = [
-            'flight',
-            'departs:scheduled',
-            'departs:status',
-            'departs:gate',
-            'arrives:scheduled',
-            'arrives:status'
-        ]
-        i = 0
-        for td in time_table.xpath('//tbody//tr//td'):
-            # etree.dump(td)
-            # print(td.xpath('span[@class="emData2"]'))
+        flg_has_exit = False
+        if not dict_time['arrives'].get('exit') is None:
+            flg_has_exit = True
+
+        idx = 1
+        td_flight = time_table.xpath('//tbody//tr//td[{idx}]'.format(idx=idx))[0]
+        flight = td_flight.text.strip()
+        flight = re.sub(r'(\s|(\u3000))+', ' ', flight)
+        dict_time['flight'] = flight
+
+        idx += 2
+
+        depart_keys = ['scheduled', 'status', 'gate']
+        for i, td in enumerate(time_table.xpath('//tbody//tr//td[position() >= {} and position() < {}]'.format(idx, idx + 3))):
             data = td.text.strip()
             data = re.sub(r'(\s|(\u3000))+', ' ', data)
-            if data != '':
-                keys = key_list[i].split(':')
-                if len(keys) == 1:
-                    dict_time[keys[0]] = data
-                else:
-                    if len(td.xpath('span[@class="emData2"]')):
-                        tmp = ' '.join([node.text.strip() for node in td.xpath('span[@class="emData2"]') if node.text.strip() != ''])
-                        data += (' {}'.format(tmp))
-                    dict_time[keys[0]][keys[1]] = data
-                i += 1
+            if len(td.xpath('span[@class="emData2"]')):
+                tmp = ' '.join([node.text.strip() for node in td.xpath('span[@class="emData2"]') if node.text.strip() != ''])
+                data += (' {}'.format(tmp))
+            dict_time['departs'][depart_keys[i]] = data
+            idx += 1
+
+        arrive_end = idx + 2
+        arrives_keys = depart_keys[:-1]
+        if flg_has_exit:
+            arrive_end += 1
+            arrives_keys.append('exit')
+
+        for i, td in enumerate(time_table.xpath('//tbody//tr//td[position() >= {} and position() < {}]'.format(idx, arrive_end))):
+            data = td.text.strip()
+            data = re.sub(r'(\s|(\u3000))+', ' ', data)
+            if len(td.xpath('span[@class="emData2"]')):
+                tmp = ' '.join([node.text.strip() for node in td.xpath('span[@class="emData2"]') if node.text.strip() != ''])
+                data += (' {}'.format(tmp))
+            dict_time['arrives'][arrives_keys[i]] = data
+
+        idx = arrive_end
+        remarks_keys = ['status', 'note']
+
+        for i, td in enumerate(time_table.xpath('//tbody//tr//td[position() >= {}]'.format(idx))):
+            data = td.text.strip()
+            data = re.sub(r'(\s|(\u3000))+', ' ', data)
+            if len(td.xpath('span')):
+                tmp = ' '.join([node.text.strip() for node in td.xpath('span') if isinstance(node.text, str) and node.text.strip() != ''])
+                data += (' {}'.format(tmp))
+            dict_time['remarks'][remarks_keys[i]] = data.strip()
 
         return dict_time
+
+class SectionMiles(object):
+    def __init__(self, xml_str):
+        self.xml_str = xml_str
+
+    def parse_section_miles(self, xml_str=None, encoding='utf-8'):
+        if not xml_str:
+            xml_str = self.xml_str
+
+        if isinstance(xml_str, str):
+            xml_str = xml_str.decode(encoding)
+
+        parsed = etree.XML(xml_str)
--- test/testParser.py
+++ test/testParser.py
@@ -4,23 +4,46 @@ import unittest, sys, os
 SRC_PATH = "/".join(os.path.abspath(__file__).split("/")[:-2])
 sys.path.append(SRC_PATH)
 
-class TestParser(unittest.TestCase):
-    def test_DomesticStatus(self):
+class TestDomesticStatus(unittest.TestCase):
+    def setUp(self):
         from src.lib.parser import DomesticStatus
+        self.Parser = DomesticStatus
+
+    def test_default(self):
         with open('test/data/test_default.html', 'r') as f:
             page = f.read()
 
-        parser = DomesticStatus(page)
+        parser = self.Parser(page)
         time_table = parser.parseTimeTable(parser.getTimeTable())
-        print(time_table)
+        self.assertDictEqual(time_table, {
+            'flight': 'JAL3537', 
+            'departs': {'scheduled': '14:10', 'status': '14:17 Departed', 'gate': '3'}, 
+            'arrives': {'scheduled': '15:55', 'status': '16:08 Arrived'}, 
+            'remarks': {'status': 'Delayed', 'note': 'due to late arrival of aircraft.'}
+        })
 
-    def test_DomesticStatus_hasError(self):
-        from src.lib.parser import DomesticStatus
+    def test_arrive_exit(self):
+        with open('test/data/test_arrive_exit.html', 'r') as f:
+            page = f.read()
+        
+        parser = self.Parser(page)
+        time_table = parser.parseTimeTable(parser.getTimeTable())
+        self.assertDictEqual(time_table, {
+            'flight': 'JAL904', 
+            'departs': {'scheduled': '11:35', 'status': '11:37 Departed', 'gate': '23'}, 
+            'arrives': {'scheduled': '13:45', 'status': '14:04 Arrived', 'exit': 'North Wing'}, 
+            'remarks': {'status': '', 'note': ''}
+        })
+
+    def test_error_1(self):
         with open('test/data/test_error_1.html', 'r') as f:
             page = f.read()
 
         with self.assertRaises(Exception):
-            parser = DomesticStatus(page)
+            parser = self.Parser(page)
+
+class TestSectionMiles(unittest.TestCase):
+    pass
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
